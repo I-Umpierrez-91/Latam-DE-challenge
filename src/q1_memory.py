36aa00dc2270e6a4ed24368fe622e09fbb6354c5
I-Umpierrez-91 <I-Umpierrez-91@users.noreply.github.com>
2023-10-29 15:31:04 -0300

    q1 Memory listo

--- src/q1_memory.py
+++ src/q1_memory.py
@@ -1,20 +1,25 @@
 from typing import List, Tuple
 from datetime import datetime
+from memory_profiler import profile
 import pandas as pd
-import csv
+import json
 
+
+@profile
 def q1_memory(file_path: str) -> List[Tuple[datetime.date, str]]:
-
-    # Leer el archivo línea por línea para ahorrar memoria
+    # Utilizo un diccionario para mantener la cuenta de tweets por usuario por dia. Uso un array para guardar los dias con mas tweets al final.
     date_counts = {}
     top_dates = []
 
-    with open('tweets.csv', 'r') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            date = pd.to_datetime(row['date'])
-            username = row['username']
-            
+    # Leo el archivo json fila a fila.
+    with open(file_path, 'r') as jsonfile:
+        for row in jsonfile:
+            reader = json.loads(row)
+            #Casteo a fecha y obtengo el nombre de usuario.
+            date = pd.to_datetime(reader['date']).date()
+            username = pd.json_normalize(reader['user']).username[0]
+
+            #Si la fecha ya estaba en el diccionario, actualizo la cuenta de tweets para ese dia. Si no, agrego la fecha y el usuario con un tweet.
             if date in date_counts:
                 if username in date_counts[date]:
                     date_counts[date][username] += 1
@@ -24,10 +29,11 @@ def q1_memory(file_path: str) -> List[Tuple[datetime.date, str]]:
                 date_counts[date] = {username: 1}
 
     # Encontrar las 10 fechas con más tweets
-    for date, user_counts in sorted(date_counts.items(), key=lambda x: len(x[1]), reverse=True)[:10]:
+    for date, user_counts in sorted(date_counts.items(), key=lambda x: sum(x[1].values()), reverse=True)[:10]:
         max_user = max(user_counts, key=user_counts.get)
         top_dates.append((date, max_user))
+    # Devolver los resultados en forma de tupla.
+    result_tuple = [tuple(row) for row in top_dates]
 
-    # Imprimir los resultados
-    for date, max_user in top_dates:
-        print(f"Fecha: {date}, Usuario con más publicaciones: {max_user}")
+    # Imprime la lista de tuplas
+    return result_tuple
